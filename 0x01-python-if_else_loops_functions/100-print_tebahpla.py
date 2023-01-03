a535916ba5b9a8b1aca39a18d9b60ed6f46d62c4
daniel <fagprero@gmail.com>
2023-01-03 13:53:26 -0800

    telpapha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(90, 63, -1):
-    print("{}".format(chr(i)), end='')
+    if i % 2 == 0:
+        print("{}".format(chr(i + 32)), end='')
+    else:
+        print("{}".format(chr(i)), end="")
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    c_str = del str[n]
+    return c_str
