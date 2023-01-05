0762709980dc23365b296910c51ae4964e672682
daniel <fagprero@gmail.com>
2023-01-05 11:50:02 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argc = len(sys.argv)
-    if argc < 2:
-        print("{} argument:\n".format(argc -1))
-    if argc == 1:
-        print("0 arguments.\n")
+argc = len(sys.argv)
+if argc < 2:
+    print("{} argument.".format(argc - 1))
+else:
+    if argc == 2:
+        print("{} arguments:".format(argc - 1))
     else:
-        for i in range(1, argc):
-            print("{} argument:\n".format(argc -1))
+        print("{} arguments:".format(argc -1))
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
