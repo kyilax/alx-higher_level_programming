3508fd10194623985873ba27a24ffe167049aebb
daniel <fagprero@gmail.com>
2023-01-05 16:52:51 -0800

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import hidden_4
 dirlist = dir(hidden_4)
 for i in range(len(dirlist)):
-    if dirlist[i][0] != '_':
+    if dirlist[i][0:2] != '__':
         print("{}".format((dirlist[i]))
 
