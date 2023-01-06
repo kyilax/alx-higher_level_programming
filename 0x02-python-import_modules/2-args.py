59e1d6cd07b5df2905338d6dcbcf122b51387303
daniel <fagprero@gmail.com>
2023-01-05 17:02:18 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ else:
     else:
         print("{} arguments:".format(argc - 1))
     for i in range(1, argc):
-        print("{}: {}".format(i, argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
