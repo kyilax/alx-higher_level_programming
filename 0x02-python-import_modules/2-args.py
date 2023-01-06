fc9c640b3f8d0b1cbb02acf07f81e271d52b6cdf
daniel <fagprero@gmail.com>
2023-01-05 17:14:19 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
 argc = len(sys.argv)
 if argc < 2:
-    print("{} argument.".format(argc - 1))
+    print("{} arguments.".format(argc - 1))
 else:
     if argc == 2:
         print("{} arguments:".format(argc - 1))
