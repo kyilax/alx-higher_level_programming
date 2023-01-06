a0364f0c70d25dd091a0394ee749a4e720ffb872
daniel <fagprero@gmail.com>
2023-01-05 17:15:45 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if argc < 2:
     print("{} arguments.".format(argc - 1))
 else:
     if argc == 2:
-        print("{} arguments:".format(argc - 1))
+        print("{} argument:".format(argc - 1))
     else:
         print("{} arguments:".format(argc - 1))
     for i in range(1, argc):
