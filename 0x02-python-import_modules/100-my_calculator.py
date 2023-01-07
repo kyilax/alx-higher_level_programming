12f240650e5cdf9a271b30dda7f15bd4516e2099
daniel <fagprero@gmail.com>
2023-01-07 01:29:53 -0800

    adv_calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul
+    from calculator_1 import add, sub, mul, div
     from sys import argv
 argc = len(argv)
 if argc < 4:
