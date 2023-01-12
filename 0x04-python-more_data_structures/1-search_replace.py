e365b37c32397506d1ccf3d07cb1f2656abe4ac6
daniel <fagprero@gmail.com>
2023-01-12 14:05:05 -0800

    replace search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_lst = my_list.copy()
-    new_lst[search] = replace
+    idx = new_lst.index(search)
+    new_lst[idx] = replace
     return new_lst
