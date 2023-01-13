24c0ec95f15dc92d733af216e9c107e115cd2831
daniel <fagprero@gmail.com>
2023-01-12 23:26:12 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,11 @@ def best_score(a_dictionary):
     c = 0
     a = ""
     if not a_dictionary.values():
+        a = None
         return None
+    elif a_dictionary == None:
+        a = None
+        return a
     else:
         for k, v in a_dictionary.items():
             if v > 0:
