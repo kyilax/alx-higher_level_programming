2febcf7dbdc397cafc22fba9a5e340e56afa72f1
daniel <fagprero@gmail.com>
2023-01-25 06:34:57 -0800

    list_div

--- 0x05-python-exceptions/3-main.py
+++ 0x05-python-exceptions/3-main.py
@@ -6,7 +6,7 @@ b = 2
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
 
-a = 12
-b = 0
+a = 0
+b = 7
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    if (len(my_list_1) != len(my_list_2)):
+        print("out of range")
+    p = []
+    for i in range(len(my_list_1)):
+        try:
+            p.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        finally:
+            pass
+    return p
