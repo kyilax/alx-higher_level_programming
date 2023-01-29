ac88a32305bf9564ebf6ad905f874d89275cba2b
daniel <fagprero@gmail.com>
2023-01-29 13:05:31 -0800

    4_square_setter_getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""defines a class Square"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """ retuns the area of a square """
+        return (self.__size * self.__size)
+    
+    @property
+    def size(self):
+        """ returns the value of size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
